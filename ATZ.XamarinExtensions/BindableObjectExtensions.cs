1511e3eaf150cb8e9a7039255e2c648663fd636c
Zimler Attila Tamás <hijaszu@icloud.com>
2017-09-01 07:28:19 +1000

    Imported BindableObjectExtensions

---
+++ ATZ.XamarinExtensions/BindableObjectExtensions.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Xamarin.Forms;
+
+namespace ATZ.XamarinExtensions
+{
+    // TODO: This class has been copied from ATZ.XFCells, remove it from there, as the goal is to extract it into this separated library.
+    public static class BindableObjectExtensions
+    {
+        public static T GetValue<T>([NotNull] this BindableObject obj, BindableProperty property, T nullValue)
+        {
+            var value = obj.GetValue(property);
+            if (value == null)
+            {
+                return nullValue;
+            }
+
+            return (T)value;
+        }
+
+    }
+}
